50ca231c5212e4f202c0e4eaab78fd5090798a3f
Max Fischer <maxfischer2781@gmail.com>
2019-11-20 20:18:31 +0100

    filter unittest

--- unittests/test_builtins.py
+++ unittests/test_builtins.py
@@ -41,3 +41,15 @@ async def test_map_aa():
         list(range(0, 10, 2))
     assert [value async for value in a.map(map_op, asyncify(range(5, 10, 2)))] == \
         list(range(10, 20, 4))
+
+
+@sync
+async def test_filter_aa():
+    async def map_op(value):
+        return value % 2 == 0
+
+    assert [value async for value in a.filter(map_op, asyncify(range(5)))] == \
+        list(range(0, 5, 2))
+    assert [value async for value in a.filter(map_op, asyncify(range(5, 10, 2)))] == []
+    assert [value async for value in a.filter(map_op, asyncify(range(4, 10, 2)))] == \
+        list(range(4, 10, 2))
