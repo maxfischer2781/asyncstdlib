4ecf2c532b7ff6d42e1466bb3e192d20ab7e21a7
Max Fischer <maxfischer2781@gmail.com>
2019-11-26 10:41:50 +0100

    black

--- unittests/test_functools.py
+++ unittests/test_functools.py
@@ -14,28 +14,18 @@ async def test_reduce():
 
     for reducer in (reduction, lambda x, y: x + y):
         for itertype in (asyncify, list):
-            assert await a.reduce(
-                reducer, itertype([0, 1])
-            ) == functools.reduce(
+            assert await a.reduce(reducer, itertype([0, 1])) == functools.reduce(
                 lambda x, y: x + y, [0, 1]
             )
             assert await a.reduce(
                 reducer, itertype([0, 1, 2, 3, 4, 0, -5])
-            ) == functools.reduce(
-                lambda x, y: x + y, [0, 1, 2, 3, 4, 0, -5]
-            )
-            assert await a.reduce(
-                reducer, itertype([1]), 23,
-            ) == functools.reduce(
+            ) == functools.reduce(lambda x, y: x + y, [0, 1, 2, 3, 4, 0, -5])
+            assert await a.reduce(reducer, itertype([1]), 23,) == functools.reduce(
                 lambda x, y: x + y, [1], 23
             )
-            assert await a.reduce(
-                reducer, itertype([12])
-            ) == functools.reduce(
+            assert await a.reduce(reducer, itertype([12])) == functools.reduce(
                 lambda x, y: x + y, [12]
             )
-            assert await a.reduce(
-                reducer, itertype([]), 42
-            ) == functools.reduce(
+            assert await a.reduce(reducer, itertype([]), 42) == functools.reduce(
                 lambda x, y: x + y, [], 42
             )
