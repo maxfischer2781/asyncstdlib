0f3c24fd0c6281bc7bcff96d470eb7b831224ff2
Max Fischer <maxfischer2781@gmail.com>
2019-12-16 16:10:05 +0100

    using neutral sphinx theme

--- docs/conf.py
+++ docs/conf.py
@@ -88,7 +88,7 @@ add_module_names = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'python_docs_theme'  # 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
