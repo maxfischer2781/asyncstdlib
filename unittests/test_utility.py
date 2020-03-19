ed612189e5ce8c604eb90a9e18e7dfd41e7c3d75
Max Fischer <maxfischer2781@gmail.com>
2020-03-19 12:55:30 +0100

    testing slot_get

---
+++ unittests/test_utility.py
@@ -0,0 +1,19 @@
+from asyncstdlib import _utility
+
+
+def test_slot_get():
+    class Slotted:
+        data = 3
+
+        def __neg__(self):
+            return - type(self).data
+
+    instance = Slotted()
+    assert _utility.slot_get(instance, "data") is instance.data
+    assert _utility.slot_get(instance, "__neg__")() == -instance
+    assert _utility.slot_get(instance, "__neg__")() == instance.__neg__()
+    data, neg = instance.data, -instance
+    instance.data = 4
+    instance.__neg__ = lambda self: 12
+    assert _utility.slot_get(instance, "data") is data
+    assert _utility.slot_get(instance, "__neg__")() == neg
