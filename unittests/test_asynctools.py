228bb8368971b7e92f03ef9e5e1ccde20a6e62a1
Max Fischer <maxfischer2781@gmail.com>
2020-05-25 12:31:40 +0200

    added test for non-generator async iterators

--- unittests/test_asynctools.py
+++ unittests/test_asynctools.py
@@ -34,3 +34,24 @@ async def test_borrow_explicitly():
     async for value in async_iterable:
         values.append(value)
     assert values == list(range(10))
+
+
+class Uncloseable:
+    def __init__(self, iterator):
+        self.iterator = iterator
+
+    def __aiter__(self):
+        return self
+
+    async def __anext__(self):
+        return await a.anext(self.iterator)
+
+
+@sync
+async def test_borrow_iterable():
+    async_iterable = Uncloseable(asyncify(range(10)))
+    values = []
+    async with a.scoped_iter(async_iterable) as a1:
+        values.append(await a.anext(a1))
+    values.append(await a.anext(a.borrow(async_iterable)))
+    assert values == [0, 1]
