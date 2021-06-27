066f09b694aa02062a0f18bc717438a2277af08a
Max Fischer <maxfischer2781@gmail.com>
2021-06-27 14:46:02 +0200

    removed dead test code

--- unittests/test_functools.py
+++ unittests/test_functools.py
@@ -35,12 +35,11 @@ async def test_cache_property_nodict():
             __slots__ = "a", "b"
 
             def __init__(self, a, b):
-                self.a = a
-                self.b = b
+                pass
 
             @a.cached_property
             async def total(self):
-                return self.a + self.b
+                pass
 
 
 @multi_sync
