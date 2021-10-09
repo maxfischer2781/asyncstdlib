af6e23ab2657cd66a8b56c6a4ed6e7424c7ee9cc
Max Fischer <maxfischer2781@gmail.com>
2021-10-09 12:36:26 +0200

    Officially support Python 3.10 (#66)
    * testing for Python 3.10
    
    * added flake8 checks for python version comparisons
    
    * added classifiers for supported versions

--- asyncstdlib/_typing.py
+++ asyncstdlib/_typing.py
@@ -7,7 +7,7 @@ This module is for internal use only. Do *not* put any new
 import sys
 from typing import TypeVar, Hashable, Union, AsyncIterable, Iterable, Callable
 
-if sys.version_info[:2] >= (3, 8):
+if sys.version_info >= (3, 8):
     from typing import Protocol, AsyncContextManager, ContextManager, TypedDict
 else:
     from typing_extensions import (
