d13fc8062291df0196281d359eabe709da0b54c2
Max Fischer <maxfischer2781@gmail.com>
2022-02-04 21:21:05 +0100

    testing different cache sizes

--- unittests/test_functools_lru.py
+++ unittests/test_functools_lru.py
@@ -1,24 +1,28 @@
+import pytest
+
 import asyncstdlib as a
 
 from .utility import sync
 
 
+@pytest.mark.parametrize("size", [0, 3, 10, None])
 @sync
-async def test_method():
+async def test_method(size):
     """Test wrapping a method"""
 
     class Counter:
         def __init__(self):
             self._count = 0
 
-        @a.lru_cache
+        @a.lru_cache(maxsize=size)
         async def count(self):
             self._count += 1
             return self._count
 
     for _instance in range(4):
         instance = Counter()
-        for _reset in range(5):
-            for _access in range(5):
-                assert _reset + 1 == await instance.count()
+        for reset in range(5):
+            for access in range(5):
+                misses = reset + 1 if size != 0 else reset * 5 + access + 1
+                assert misses == await instance.count()
             instance.count.cache_clear()
