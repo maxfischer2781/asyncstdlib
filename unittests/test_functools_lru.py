4f2c9b3bd88b947790ab8a8dfa7533cf1f46cf27
Max Fischer <maxfischer2781@gmail.com>
2022-02-08 18:11:08 +0100

    added unittests for classmethod

--- unittests/test_functools_lru.py
+++ unittests/test_functools_lru.py
@@ -19,8 +19,32 @@ async def test_method(size):
             self._count += 1
             return self._count
 
+    await _test_counting(size, Counter)
+
+
+@pytest.mark.parametrize("size", [0, 3, 10, None])
+@sync
+async def test_classmethod(size):
+    """Test wrapping a method"""
+
+    class Counter:
+        _count = 0
+
+        def __init__(self):
+            type(self)._count = 0
+
+        @classmethod
+        @a.lru_cache(maxsize=size)
+        async def count(cls):
+            cls._count += 1
+            return cls._count
+
+    await _test_counting(size, Counter)
+
+
+async def _test_counting(size, counter_type):
     for _instance in range(4):
-        instance = Counter()
+        instance = counter_type()
         for reset in range(5):
             for access in range(5):
                 misses = reset + 1 if size != 0 else reset * 5 + access + 1
