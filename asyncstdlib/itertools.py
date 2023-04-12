ecbe65dc758a6929076e2875bc6e67e139e409e2
Max Fischer <maxfischer2781@gmail.com>
2023-04-12 15:43:59 +0200

    typing fix

--- asyncstdlib/itertools.py
+++ asyncstdlib/itertools.py
@@ -153,7 +153,7 @@ class chain(AsyncIterator[T]):
     __slots__ = ("_impl",)
 
     def __init__(self, *iterables: AnyIterable[T]):
-        async def impl() -> AsyncIterator[T]:
+        async def impl() -> AsyncGenerator[T, None]:
             for iterable in iterables:
                 async with ScopedIter(iterable) as iterator:
                     async for item in iterator:
