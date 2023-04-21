baa9b4891060111ddc4da0c2e7cbe30ea143e47a
Max Fischer <maxfischer2781@gmail.com>
2023-04-21 17:42:39 +0200

    black stubs

--- asyncstdlib/_lrucache.pyi
+++ asyncstdlib/_lrucache.pyi
@@ -1,5 +1,16 @@
 from ._typing import AC, Protocol, R as R, TypedDict
-from typing import Any, Awaitable, Callable, Dict, Hashable, NamedTuple, Optional, Tuple, Union, overload
+from typing import (
+    Any,
+    Awaitable,
+    Callable,
+    Dict,
+    Hashable,
+    NamedTuple,
+    Optional,
+    Tuple,
+    Union,
+    overload,
+)
 
 class CacheInfo(NamedTuple):
     hits: int
@@ -17,13 +28,13 @@ class LRUAsyncCallable(Protocol[AC]):
     def __get__(
         self: LRUAsyncCallable[Callable[..., Awaitable[R]]],
         instance: None,
-        owner: Optional[type]
+        owner: Optional[type],
     ) -> LRUAsyncCallable[Callable[..., Awaitable[R]]]: ...
     @overload
     def __get__(
         self: LRUAsyncCallable[Callable[..., Awaitable[R]]],
         instance: Any,
-        owner: Optional[type]
+        owner: Optional[type],
     ) -> LRUAsyncBoundCallable[Callable[..., Awaitable[R]]]: ...
     @property
     def __wrapped__(self) -> AC: ...
@@ -46,8 +57,9 @@ class LRUAsyncBoundCallable(LRUAsyncCallable[AC]):
     def cache_discard(self, *args: Any, **kwargs: Any) -> None: ...
     def __getattr__(self, name: str) -> Any: ...
 
-
 @overload
 def lru_cache(maxsize: AC, typed: bool = ...) -> LRUAsyncCallable[AC]: ...
 @overload
-def lru_cache(maxsize: Optional[int] = ..., typed: bool = ...) -> Callable[[AC], LRUAsyncCallable[AC]]: ...
+def lru_cache(
+    maxsize: Optional[int] = ..., typed: bool = ...
+) -> Callable[[AC], LRUAsyncCallable[AC]]: ...
