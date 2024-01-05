c8298a3d5f4e131efedeaae2d99f92501c6e2675
Max Fischer <maxfischer2781@gmail.com>
2024-01-05 14:10:12 +0100

    remove typing backports

--- asyncstdlib/_typing.py
+++ asyncstdlib/_typing.py
@@ -4,7 +4,6 @@ Helper module to simplify version specific typing imports
 This module is for internal use only. Do *not* put any new
 "async typing" definitions here.
 """
-import sys
 from typing import (
     TypeVar,
     Hashable,
@@ -16,15 +15,7 @@ from typing import (
     Awaitable,
 )
 
-if sys.version_info >= (3, 8):
-    from typing import Protocol, AsyncContextManager, ContextManager, TypedDict
-else:
-    from typing_extensions import (
-        Protocol,
-        AsyncContextManager,
-        ContextManager,
-        TypedDict,
-    )
+from typing import Protocol, AsyncContextManager, ContextManager, TypedDict
 
 __all__ = [
     "Protocol",
