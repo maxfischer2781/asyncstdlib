1ffdaced72d2432505cb053d4ebd527fcc0d4f50
Max Fischer <maxfischer2781@gmail.com>
2024-01-06 10:46:56 +0100

    remove deprecated test

--- unittests/test_helpers.py
+++ unittests/test_helpers.py
@@ -1,25 +1,6 @@
-from asyncstdlib import _utility
 from asyncstdlib import _core
 
 from .utility import sync
-
-
-def test_slot_get():
-    class Slotted:
-        data = 3
-
-        def __neg__(self):
-            return -type(self).data
-
-    instance = Slotted()
-    assert _utility.slot_get(instance, "data") is instance.data
-    assert _utility.slot_get(instance, "__neg__")() == -instance
-    assert _utility.slot_get(instance, "__neg__")() == instance.__neg__()
-    data, neg = instance.data, -instance
-    instance.data = 4
-    instance.__neg__ = lambda self: 12
-    assert _utility.slot_get(instance, "data") is data
-    assert _utility.slot_get(instance, "__neg__")() == neg
 
 
 @sync
