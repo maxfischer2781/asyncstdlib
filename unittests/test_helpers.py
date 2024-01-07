390e091598c164ecb67dee61ad456eb26aefa44d
Max Fischer <maxfischer2781@gmail.com>
2024-01-07 10:14:00 +0100

    expanded test case

--- unittests/test_helpers.py
+++ unittests/test_helpers.py
@@ -5,6 +5,8 @@ from .utility import sync
 
 @sync
 async def test_scoped_iter_graceful():
+    """Test that ScopedIter handlers non-generator iterables"""
+
     class AIterator:
         async def __anext__(self):
             return 1
@@ -20,5 +22,7 @@ async def test_scoped_iter_graceful():
     async with _core.ScopedIter(async_iterable) as async_iterator:
         # test that no error from calling the missing `aclose` is thrown
         assert async_iterator is not async_iterable
+        # check that the iterator satisfies iter(iterator) is iterator
+        assert _core.aiter(async_iterator) is async_iterator
         assert (await async_iterator.__anext__()) == 1
     assert (await async_iterator.__anext__()) == 1
