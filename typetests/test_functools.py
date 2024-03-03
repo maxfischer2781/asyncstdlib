1664819d6c0e4835ceb779272bbf10707e68d5f7
Max Fischer <maxfischer2781@gmail.com>
2024-03-03 10:42:32 +0100

    desire full parameter support

--- typetests/test_functools.py
+++ typetests/test_functools.py
@@ -15,9 +15,14 @@ class TestLRUMethod:
     """
     Test that `lru_cache` works on methods
     """
+
     @lru_cache()
-    async def cached(self) -> int:
-        return 1
+    async def cached(self, a: int = 0) -> int:
+        return a
 
     async def test_implicit_self(self) -> int:
         return await self.cached()
+
+    async def test_method_parameters(self) -> int:
+        await self.cached("wrong parameter type")  # type: ignore[arg-type]
+        return await self.cached(12)
